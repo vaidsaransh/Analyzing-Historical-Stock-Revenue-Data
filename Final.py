392a01fe38682f8a6b539b4981327ef9e6937814
Saransh Vaid <112588232+vaidsaransh@users.noreply.github.com>
2023-05-08 02:07:04 -0400

    Add files via upload

---
+++ Final.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install yfinance')
+#!pip install pandas
+#!pip install requests
+get_ipython().system('pip install bs4')
+#!pip install plotly
+
+
+# In[2]:
+
+
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
+# ## Define Graphing Function
+# 
+
+# In this section, we define the function `make_graph`. You don't have to know how the function works, you should only care about the inputs. It takes a dataframe with stock data (dataframe must contain Date and Close columns), a dataframe with revenue data (dataframe must contain Date and Revenue columns), and the name of the stock.
+# 
+
+# In[3]:
+
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data.Date, infer_datetime_format=True), y=stock_data.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data.Date, infer_datetime_format=True), y=revenue_data.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+
+# ## Question 1: Use yfinance to Extract Stock Data
+# 
+
+# Using the `Ticker` function enter the ticker symbol of the stock we want to extract data on to create a ticker object. The stock is Tesla and its ticker symbol is `TSLA`.
+# 
+
+# In[4]:
+
+
+tesla = yf.Ticker("TSLA")
+
+
+# Using the ticker object and the function `history` extract stock information and save it in a dataframe named `tesla_data`. Set the `period` parameter to `max` so we get information for the maximum amount of time.
+# 
+
+# In[5]:
+
+
+tesla_data = tesla.history(period="max")
+
+
+# **Reset the index** using the `reset_index(inplace=True)` function on the tesla_data DataFrame and display the first five rows of the `tesla_data` dataframe using the `head` function. Take a screenshot of the results and code from the beginning of Question 1 to the results below.
+# 
+
+# In[6]:
+
+
+tesla_data.reset_index(inplace=True)
+tesla_data.head(10)
+
+
+# ## Question 2: Use Webscraping to Extract Tesla Revenue Data
+# 
+
+# Use the `requests` library to download the webpage [https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue](https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue?cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ). Save the text of the response as a variable named `html_data`.
+# 
+
+# In[7]:
+
+
+url= "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+html_data=requests.get(url).text
+
+
+# Parse the html data using `beautiful_soup`.
+# 
+
+# In[8]:
+
+
+soup = BeautifulSoup(html_data,"html5lib")
+
+
+# Using beautiful soup extract the table with `Tesla Quarterly Revenue` and store it into a dataframe named `tesla_revenue`. The dataframe should have columns `Date` and `Revenue`. Make sure the comma and dollar sign is removed from the `Revenue` column. 
+# 
+
+# In[9]:
+
+
+tesla_revenue= pd.read_html(url, match="Tesla Quarterly Revenue", flavor='bs4')[0]
+tesla_revenue=tesla_revenue.rename(columns = {'Tesla Quarterly Revenue(Millions of US $)': 'Date', 'Tesla Quarterly Revenue(Millions of US $).1': 'Revenue'}, inplace = False)
+tesla_revenue["Revenue"] = tesla_revenue["Revenue"].str.replace(",","").str.replace("$","")
+tesla_revenue.head()
+
+
+# <details><summary>Click here if you need help removing the dollar sign and comma</summary>
+# 
+# ```
+#     
+# If you parsed the HTML table by row and column you can use the replace function on the string
+#     
+#     revenue = col[1].text.replace("$", "").replace(",", "")
+#     
+# If you use the read_html function you can use the replace function on the string representation of the column
+# 
+#     tesla_revenue["Revenue"] = tesla_revenue["Revenue"].str.replace("$", "").str.replace(",", "")
+# 
+# ```
+# 
+# </details>
+# 
+
+# Remove the rows in the dataframe that are empty strings or are NaN in the Revenue column. Print the entire `tesla_revenue` DataFrame to see if you have any.
+# 
+
+# In[10]:
+
+
+tesla_revenue
+
+
+# <details><summary>Click here if you need help removing the Nan or empty strings</summary>
+# 
+# ```
+#     
+# If you have NaN in the Revenue column
+#     
+#     tesla_revenue.dropna(inplace=True)
+#     
+# If you have emtpty string in the Revenue column
+# 
+#     tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+# 
+# ```
+# 
+# </details>
+# 
+
+# Display the last 5 row of the `tesla_revenue` dataframe using the `tail` function. Take a screenshot of the results.
+# 
+
+# In[12]:
+
+
+tesla_revenue.dropna(inplace=True)
+tesla_revenue.tail()
+
+
+# ## Question 3: Use yfinance to Extract Stock Data
+# 
+
+# Using the `Ticker` function enter the ticker symbol of the stock we want to extract data on to create a ticker object. The stock is GameStop and its ticker symbol is `GME`.
+# 
+
+# In[13]:
+
+
+gamestop = yf.Ticker("GME")
+
+
+# Using the ticker object and the function `history` extract stock information and save it in a dataframe named `gme_data`. Set the `period` parameter to `max` so we get information for the maximum amount of time.
+# 
+
+# In[14]:
+
+
+gme_data=gamestop.history(period="max")
+
+
+# **Reset the index** using the `reset_index(inplace=True)` function on the gme_data DataFrame and display the first five rows of the `gme_data` dataframe using the `head` function. Take a screenshot of the results and code from the beginning of Question 3 to the results below.
+# 
+
+# In[15]:
+
+
+gme_data.reset_index(inplace=True)
+gme_data.head()
+
+
+# ## Question 4: Use Webscraping to Extract GME Revenue Data
+# 
+
+# Use the `requests` library to download the webpage [https://www.macrotrends.net/stocks/charts/GME/gamestop/revenue](https://www.macrotrends.net/stocks/charts/GME/gamestop/revenue?cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ). Save the text of the response as a variable named `html_data`.
+# 
+
+# In[16]:
+
+
+url="https://www.macrotrends.net/stocks/charts/GME/gamestop/revenue"
+html_data=requests.get(url).text
+
+
+# Parse the html data using `beautiful_soup`.
+# 
+
+# In[17]:
+
+
+soup = BeautifulSoup(html_data,"html5lib")
+
+
+# Using beautiful soup extract the table with `GameStop Quarterly Revenue` and store it into a dataframe named `gme_revenue`. The dataframe should have columns `Date` and `Revenue`. Make sure the comma and dollar sign is removed from the `Revenue` column using a method similar to what you did in Question 2.
+# 
+
+# In[18]:
+
+
+gme_revenue= pd.read_html(url, match="GameStop Quarterly Revenue", flavor='bs4')[0]
+gme_revenue=gme_revenue.rename(columns = {'GameStop Quarterly Revenue(Millions of US $)': 'Date', 'GameStop Quarterly Revenue(Millions of US $).1': 'Revenue'}, inplace = False)
+gme_revenue["Revenue"] = gme_revenue["Revenue"].str.replace(",","").str.replace("$","")
+
+
+# Display the last five rows of the `gme_revenue` dataframe using the `tail` function. Take a screenshot of the results.
+# 
+
+# In[19]:
+
+
+gme_revenue.dropna(inplace=True)
+gme_revenue.tail()
+
+
+# ## Question 5: Plot Tesla Stock Graph
+# 
+
+# Use the `make_graph` function to graph the Tesla Stock Data, also provide a title for the graph. The structure to call the `make_graph` function is `make_graph(tesla_data, tesla_revenue, 'Tesla')`
+# 
+
+# In[20]:
+
+
+make_graph(tesla_data, tesla_revenue, 'Tesla Stock Data Graph')
+
+
+# ## Question 6: Plot GameStop Stock Graph
+# 
+
+# Use the `make_graph` function to graph the GameStop Stock Data, also provide a title for the graph. The structure to call the `make_graph` function is `make_graph(gme_data, gme_revenue, 'GameStop')`.
+# 
+
+# In[21]:
+
+
+make_graph(gme_data, gme_revenue, 'GameStop Stock Data Graph')
+
+
+# <h2>About the Authors:</h2> 
+# 
+# <a href="https://www.linkedin.com/in/joseph-s-50398b136/">Joseph Santarcangelo</a> has a PhD in Electrical Engineering, his research focused on using machine learning, signal processing, and computer vision to determine how videos impact human cognition. Joseph has been working for IBM since he completed his PhD.
+# 
+# Azim Hirjani
+# 
+
+# ## Change Log
+# 
+# | Date (YYYY-MM-DD) | Version | Changed By    | Change Description        |
+# | ----------------- | ------- | ------------- | ------------------------- |
+# | 2020-11-10        | 1.1     | Malika Singla | Deleted the Optional part |
+# | 2020-08-27        | 1.0     | Malika Singla | Added lab to GitLab       |
+# 
+# <hr>
+# 
+# ## <h3 align="center"> © IBM Corporation 2020. All rights reserved. <h3/>
+# 
+# <p>
+# 
